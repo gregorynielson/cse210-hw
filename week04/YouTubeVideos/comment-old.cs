f4126a77a15cfd38da73d4e646e166c7d03f7fdb
gregorynielson <gnielson97@gmail.com>
2026-04-04 18:00:19 -0400

    old changes

---
+++ week04/YouTubeVideos/comment-old.cs
@@ -0,0 +1,39 @@
+// using System.Collections.Generic;
+
+// public class Comment {
+
+//     public Video _video;
+
+//     public string _name;
+//     public string _comment;
+//     // public List<string> _comments = new List<string>()
+//     // {
+//     //     "Doc_The_Dwarf - I'm leaving this comment with a lot of insight and constructive critiscism.",
+//     //     "Bashful_Dwarf - I am embarrassed to be leaving this comment and don't know if I should have in the first place.",
+//     //     "Dopey_Dwarf - I am leaving this silly comment with what I found amusing about the video.",
+//     //     "Grumpy_Dwarf - I am leaving this comment to let you know that I did not like the video and it made me upset.",
+//     //     "Happy_Dwarf - I am leaving this joyful comment and sharing how it made me smile.",
+//     //     "Sleepy_Dwarf - I am falling asleep while leaving this comment and should have gone to bed hours ago.",
+//     //     "Sneezy_Dwarf - I am home sick so I have tons of free time to leave comments on all my favorite videos."
+//     // };
+
+//     public Comment(string name, string comment)
+//     {
+//         _name = name;
+//         _comment = comment;
+//     }
+
+    // public void NumberOfComments()
+    // {
+    //     int _numComments = 0;
+    //     Random random = new Random();
+
+    //     while (_numComments < 3) {
+    //         int _randomComment = random.Next(0, _comments.Count);
+    //         Console.WriteLine(_comments[_randomComment]);
+    //         _numComments++;
+    //     }
+
+    // }
+    
+// }
