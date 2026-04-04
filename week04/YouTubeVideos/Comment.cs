af639834f4567c92cd0d7f94b89dc1f7649e760b
gregorynielson <gnielson97@gmail.com>
2026-04-04 18:09:42 -0400

    fixed capitalization

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+public class Comment {
+
+    public Video _video;
+
+    public string _name;
+    public string _comment;
+    
+
+    public Comment(string name, string comment)
+    {
+        _name = name;
+        _comment = comment;
+    }
+    //trying to fix title case
+}
