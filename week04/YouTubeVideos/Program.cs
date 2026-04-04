8b4adf03857eefb296677fccffc19963a5ae7b86
gregorynielson <gnielson97@gmail.com>
2026-04-04 18:36:30 -0400

    removing files

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -56,26 +56,5 @@ class Program
             Console.WriteLine("");
         }
         
-
-        // Video vid2 = new Video("How 'Cars' Was Made", "Pixar Studios", 1245, 4);
-        // Comment com2 = new Comment(vid2);
-        // vid2.DisplayVideoInfo();
-        // vid2.SaveVideoAndComment();
-        // com2.DisplayComments();
-        // Console.WriteLine();
-
-        // Video vid3 = new Video("Best of Bugs Bunny and Elmer Fudd", "Looney Tunes", 835, 4);
-        // Comment com3 = new Comment(vid3);
-        // vid3.DisplayVideoInfo();
-        // vid3.SaveVideoAndComment();
-        // com3.DisplayComments();
-        // Console.WriteLine("");
-
-        // Video vid4 = new Video("Everytime Tom Caught Jerry", "Hanna Barbera", 945, 3);
-        // Comment com4 = new Comment(vid4);
-        // vid4.DisplayVideoInfo();
-        // vid4.SaveVideoAndComment();
-        // com4.DisplayComments();
-        // Console.WriteLine();
     }
 }
