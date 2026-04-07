9c0de67d6994af16ed525f6b91bcbf5d03dd6f1f
gregorynielson <gnielson97@gmail.com>
2026-04-07 11:23:55 -0400

    week 5 homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment asmnt1 = new Assignment("Greg", "math");
+        Console.WriteLine(asmnt1.GetSummary());
+        Console.WriteLine("");
+
+        MathAssignment math1 = new MathAssignment("Adrian", "Geometry", "4.3", "9-10");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment write1 = new WritingAssignment("Bryce", "Offensive Strategies", "Reading Defensive Structures");
+        Console.WriteLine(write1.GetSummary());
+        Console.WriteLine(write1.GetWritingInformation());
     }
 }
